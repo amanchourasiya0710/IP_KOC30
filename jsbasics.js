98c5ebfcde7ffdb230647eeba3955bb3783a6dff
nandinisingh1004 <nandinisingh100499@gmail.com>
2022-12-24 19:17:45 +0530

    done

---
+++ jsbasics.js
@@ -0,0 +1,51 @@
+// <script src="scriptfile.js"></script> 
+
+// alert("JavaScript ke lie excited");
+// This is a comment in JavaScript
+console.log("My first JavaScript print statement");
+
+// How to declare variables in JavaScript
+// let, const, var
+// let v = 12;
+// console.log(v);
+
+// v = "anything";
+// console.log(v);
+
+// const cn = "hello";
+// console.log(cn);
+
+// cn  = "hello world";
+
+// functions in javascript
+// Declaring a function
+function greeting(name, age) {
+  console.log("Name:", name, "Age:", age);
+  age = age * 2;
+  console.log("Double age:", age);
+}
+
+// Calling a function
+greeting("Ranjeet", 16);
+
+// using let and const keywords, local variables are created.
+// function localFunction() {
+//     let i = 12;
+//     const j = 13;
+//     console.log(i, j);
+// }
+
+// console.log(j);
+// console.log(i);
+
+// JavaScript while loop
+// let i = 1;
+// while (i < 10) {
+//     console.log(i);
+//     i = i + 1;
+// }
+
+// JavaScript for loop
+for (let i = 0; i < 10; i = i + 2) {
+  console.log(i);
+}
